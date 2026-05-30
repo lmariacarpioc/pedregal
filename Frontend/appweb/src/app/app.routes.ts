81afc29228490cf0efe1f451514aa323a39a9767
Ana20 <U23206449@utp.edu.pe>
2026-05-30 01:52:05 -0500

    config1

--- Frontend/appweb/src/app/app.routes.ts
+++ Frontend/appweb/src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { LoginPage } from './pages/login/login.page';
 import { DashboardPage } from './pages/dashboard/dashboard.page';
 import { StaffPage } from './pages/staff/staff.page';
 import { Menu } from './pages/menu/menu';
+import{Configuracion} from './pages/configuracion/configuracion';
+import { Reportes } from './pages/reportes/reportes';
 
 export const routes: Routes = [
     {
@@ -20,7 +22,9 @@ export const routes: Routes = [
    component: Menu,
    children: [
 {path: 'dashboard', component: DashboardPage},
-{path: 'staff', component: StaffPage}
+{path: 'staff', component: StaffPage},
+{path: 'configuracion', component: Configuracion},
+{path: 'reportes', component: Reportes}
    ]
    }
   
--- Frontend/appweb/src/app/pages/dashboard/dashboard.page.ts
+++ Frontend/appweb/src/app/pages/dashboard/dashboard.page.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './dashboard.page.html',
   styleUrl: './dashboard.page.css',
 })
-export class DashboardPage {}
+export class DashboardPage implements OnInit {
+  //DATOS DE PRUEBAS 
+  alertasPersonal = [
+    { 
+      nombre: 'Juan Carlos Ramos', 
+      dni: '45678912', 
+      lote: 'Lote 12 - Fundo Ica', 
+      labor: 'Cosecha de Uva', 
+      unidadesHora: 5, 
+      rendimiento: 50, 
+      impactoEconomico: 180.00 
+    },
+    { 
+      nombre: 'María Elías Espinoza', 
+      dni: '71234568', 
+      lote: 'Lote 05 - Fundo Ica', 
+      labor: 'Poda de Formación', 
+      unidadesHora: 6, 
+      rendimiento: 60, 
+      impactoEconomico: 145.50 
+    },
+    { 
+      nombre: 'Carlos Mendoza Loza', 
+      dni: '40897654', 
+      lote: 'Lote 14 - Los Brujos', 
+      labor: 'Cosecha de Uva', 
+      unidadesHora: 8, 
+      rendimiento: 80, 
+      impactoEconomico: 0.00 
+    },
+    { 
+      nombre: 'Ana Flores Quispe', 
+      dni: '42781390', 
+      lote: 'Lote 02 - Fundo Ica', 
+      labor: 'Riego / Fertirriego', 
+      unidadesHora: 11, 
+      rendimiento: 110, 
+      impactoEconomico: 0.00 
+    }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+}
