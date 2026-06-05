7e4fc072baf9e0bb2a2d48411dea468095b54a40
Ana20 <U23206449@utp.edu.pe>
2026-06-05 18:55:35 -0500

    config-login

--- Frontend/appweb/src/app/pages/login/login.page.ts
+++ Frontend/appweb/src/app/pages/login/login.page.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule,Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, FormsModule],
   templateUrl: './login.page.html',
   styleUrl: './login.page.css',
 })
 export class LoginPage {
-  // Aquí irá la lógica de validación de credenciales
+  email = '';
+  password = '';
+  errorMsg = '';
+
+  // Credenciales hardcodeadas (para demo sin backend)
+  readonly USUARIO_VALIDO = 'admin@pedregal.pe';
+  readonly PASS_VALIDA = 'pedregal2024';
+
+  constructor(private router: Router) {}
+
+  login(): void {
+    if (!this.email || !this.password) {
+      this.errorMsg = 'Completa todos los campos.';
+      return;
+    }
+    if (this.email !== this.USUARIO_VALIDO || this.password !== this.PASS_VALIDA) {
+      this.errorMsg = 'Usuario o contraseña incorrectos.';
+      return;
+    }
+    this.errorMsg = '';
+    this.router.navigate(['/dashboard']);
+  }
 }
